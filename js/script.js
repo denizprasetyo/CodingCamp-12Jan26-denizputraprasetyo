75de56123edc3d2894bf53621d3ea6f8f26282db
denizprasetyo <denizprasetyo.04@gmail.com>
2026-01-16 11:23:59 +0700

    89% Progress

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// let user_response = prompt("Masukkan nama:");
+// alert("Selamat Datang " + user_respone +" di Website Saya");
+
+// if (user_response === null || user_response.trin() === "") {
+//     user_response = "Guest";
+// }
